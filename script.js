1e011cb2021d5cff041a8fef1c2cbf78df8f1c30
salman <salmanfaris10072005@gmail.com>
2025-06-09 15:24:07 +0700

    fix logic by add localstorage

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
     function signin(){
         Emailsigin =  document.getElementById("Email").value;
         passwordsigin =  document.getElementById("Pass").value;
+        localStorage.setItem("email", Emailsigin)
+        localStorage.setItem("pass",passwordsigin)
+
         alert(`Email = ${Emailsigin} Password = ${passwordsigin}}`);
         alert("berhasil signin");
         window.location.href = "login.html";
@@ -11,10 +14,10 @@
     function LogIn(){
         let Email =  document.getElementById("Email").value;
         let password =  document.getElementById("Pass").value;
-        alert(`Email = ${Emailsigin} Password = ${passwordsigin}`);
+        alert(`Email = ${localStorage.getItem("email")} Password = ${localStorage.getItem("pass")}`);
+        
 
-
-        if(Email === Emailsigin && password === passwordsigin){
+        if(Email === localStorage.getItem("email") && password === localStorage.getItem("pass")){
             alert(`Email = ${Email} Password = ${password}`)
         
         }else{
